8a976da0bb964ca439181391e5b7e4503d9e30bc
Donovan <dlbm3@hotmail.com>
2015-08-23 21:56:20 -0700

    Added DB and DAL projects

---
+++ src/PeopleTracker.DAL/Models.cs
@@ -0,0 +1,21 @@
+﻿namespace PeopleTracker.DAL
+{
+   using System;
+   using System.Data.Entity;
+   using System.ComponentModel.DataAnnotations.Schema;
+   using System.Linq;
+
+   public partial class Models : DbContext
+   {
+      public Models()
+          : base("name=Models")
+      {
+      }
+
+      public virtual DbSet<Person> People { get; set; }
+
+      protected override void OnModelCreating(DbModelBuilder modelBuilder)
+      {
+      }
+   }
+}
---
+++ src/PeopleTracker.Database/dbo/Tables/People.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[People] (
+    [ID]        INT           IDENTITY (1, 1) NOT NULL,
+    [FirstName] NVARCHAR (50) NOT NULL,
+    [LastName]  NVARCHAR (50) NOT NULL,
+    CONSTRAINT [PK_People] PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
