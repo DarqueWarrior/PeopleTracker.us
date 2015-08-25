d81bfe4a0a48b8b3ce370e00706155fcd2b991f6
Donovan <dlbm3@hotmail.com>
2015-08-24 21:35:57 -0700

    Add middle name.

--- src/PeopleTracker.Preview/Models/Person.cs
+++ src/PeopleTracker.Preview/Models/Person.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,8 +10,13 @@ namespace WebApplication1.Models
    {
       public int ID { get; set; }
 
+      [Display(Name = "First Name")]
       public string FirstName { get; set; }
 
+      [Display(Name = "Last Name")]
       public string LastName { get; set; }
+
+      [Display(Name = "Middle Name")]
+      public string MiddleName { get; set; }
    }
 }
