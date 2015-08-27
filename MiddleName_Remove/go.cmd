8b25a546d11133b4f0d8bb4265578a1558f4e0c0
Donovan <dlbm3@hotmail.com>
2015-08-26 17:43:50 -0700

    Added the delete sql commands to go.cmd

--- MiddleName_Remove/go.cmd
+++ MiddleName_Remove/go.cmd
@@ -7,3 +7,7 @@ git add --all
 git commit -m "Removing Middle name"
 
 git push
+
+sqlcmd -S peopletracker.database.windows.net -U dbrown -P P2ssw0rd -N -d PeopleTrackerDB-dev "delete from people where FirstName <> 'Dev' OR LastName <> 'User';"
+sqlcmd -S peopletracker.database.windows.net -U dbrown -P P2ssw0rd -N -d PeopleTrackerDB-qa "delete from people where FirstName <> 'QA' OR LastName <> 'User';"
+sqlcmd -S peopletracker.database.windows.net -U dbrown -P P2ssw0rd -N -d PeopleTrackerDB "delete from people where FirstName <> 'Prod' OR LastName <> 'User';"
