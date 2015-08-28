7b3f336918750b76ae782dfcf9c4225ae224f0b1
Donovan <dlbm3@hotmail.com>
2015-08-27 19:09:27 -0700

    Fixed sql cmd call

--- MiddleName_Remove/go.cmd
+++ MiddleName_Remove/go.cmd
@@ -8,6 +8,6 @@ git commit -m "Removing Middle name"
 
 git push
 
-sqlcmd -S peopletracker.database.windows.net -U dbrown -P P2ssw0rd -N -d PeopleTrackerDB-dev "delete from people where FirstName <> 'Dev' OR LastName <> 'User';"
-sqlcmd -S peopletracker.database.windows.net -U dbrown -P P2ssw0rd -N -d PeopleTrackerDB-qa "delete from people where FirstName <> 'QA' OR LastName <> 'User';"
-sqlcmd -S peopletracker.database.windows.net -U dbrown -P P2ssw0rd -N -d PeopleTrackerDB "delete from people where FirstName <> 'Prod' OR LastName <> 'User';"
+sqlcmd -S peopletracker.database.windows.net -U dbrown -P P2ssw0rd -N -d PeopleTrackerDB-dev -Q "delete from people where FirstName <> 'Dev' OR LastName <> 'User';"
+sqlcmd -S peopletracker.database.windows.net -U dbrown -P P2ssw0rd -N -d PeopleTrackerDB-qa -Q "delete from people where FirstName <> 'QA' OR LastName <> 'User';"
+sqlcmd -S peopletracker.database.windows.net -U dbrown -P P2ssw0rd -N -d PeopleTrackerDB -Q "delete from people where FirstName <> 'Prod' OR LastName <> 'User';"
